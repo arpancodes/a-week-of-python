7ae718f45ede7edea30ae2e1a4ec5a99b76c6c38
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-05-04 22:15:13 +0530

    outline summary for day 1

---
+++ DAY - 1 - OOP/oop.py
@@ -0,0 +1,19 @@
+# class is blueprint
+
+# we can instantiate multiple "unique" object from a single class
+
+# class contains properties and methods
+
+# constructor or "__init__"
+
+# 4 pillars of OOP:
+
+# 1. Encapsulation
+
+# 2. Abstraction
+
+# 3. Inheritance
+
+# 4. Polymorphism
+
+# BONUS: dunder methods and multiple inheritance, mro
